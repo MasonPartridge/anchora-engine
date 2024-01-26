613b66e4d1e64ec89f95b97b2d13db9659a94a5b
MasonPartridge <masonepartridge@gmail.com>
2024-01-26 13:39:26 -0800

    chore(ShaderUtils): add ShaderUtils error checking

--- app/src/main/java/anchora/engine/app/ShaderUtils.java
+++ app/src/main/java/anchora/engine/app/ShaderUtils.java
@@ -4,17 +4,30 @@ import org.lwjgl.opengl.GL20;
 
 public class ShaderUtils {
 
-    public static int loadShader(int type, String shaderSource, String shaderName) {
+    
+    public static int loadShader(int type,
+            String shaderSource, String shaderName) {
+
+        // Check if inputs are valid
+        if (shaderSource == null || shaderName == null || type == 0) {
+            throw new IllegalArgumentException("ShaderUtils: Invalid shader inputs.");
+        }
+
         // Create shader object
         int shader = GL20.glCreateShader(type);
+        if (shader == 0) {
+            throw new RuntimeException("ShaderUtils: Failed to create shader: "
+                    + shaderName);
+        }
         GL20.glShaderSource(shader, shaderSource);
         GL20.glCompileShader(shader);
 
         // Check for errors
         if (GL20.glGetShaderi(shader, GL20.GL_COMPILE_STATUS) == GL20.GL_FALSE) {
-            System.err.println("ShaderUtils: Could not compile " + shaderName + " shader. Reason:");
-            System.err.println(GL20.glGetShaderInfoLog(shader));
-            System.exit(-1);
+            throw new RuntimeException("ShaderUtils: Could not compile "
+                    + shaderName + " shader. Reason:\n"
+                    + GL20.glGetShaderInfoLog(shader));
+
         }
 
         return shader;
