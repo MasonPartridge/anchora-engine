6b6c13c149701d474746fe1ef2976a3b8778fff8
MasonPartridge <masonepartridge@gmail.com>
2024-01-26 15:37:31 -0800

    style(vertex): change variable name

--- app/bin/main/shaders/vertex.glsl
+++ app/bin/main/shaders/vertex.glsl
@@ -2,9 +2,9 @@
 layout (location = 0) in vec3 aPos;
 layout (location = 1) in vec4 aColor;
 
-out vec4 FragColor;
+out vec4 vertexColor;
 
 void main() {
     gl_Position = vec4(aPos, 1.0);
-    FragColor = aColor;
+    vertexColor = aColor;
 }
